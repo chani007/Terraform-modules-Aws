da0e7f42648d009da10457a6ba30adb0b0666980
chani007 <80414922+chani007@users.noreply.github.com>
2021-04-13 19:12:45 +0530

    Create main.tf

---
+++ Modules/Network/Igw/main.tf
@@ -0,0 +1,9 @@
+  
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = var.vpc_id
+  tags = {
+      Name = format("${var.environment}-%s",var.igw_name)
+	  Environment = var.environment
+ 	  Terraformed = "True"
+  }
+}
